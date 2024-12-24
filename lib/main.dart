b4539adaf6f1006d972875da34aa3a583352ff2f
riez69 <58677416+riez69@users.noreply.github.com>
2024-12-24 11:36:33 +0800

    menampilkan file berhasil

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart'; // Import the services package
 import 'package:file_picker/file_picker.dart';
+import 'package:pdf_viewer/pdfviewpage.dart';
 import 'function.dart';
 
 void main() {
@@ -41,11 +42,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _displayedFileCount = 10; // Jumlah file yang ditampilkan
   @override
   void initState() {
     super.initState();
     requestPermissions(); // Meminta izin saat aplikasi dibuka
-    print(findAllPdfFiles());
   }
 
   @override
@@ -61,28 +62,61 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             Expanded(
                 child: FutureBuilder(
-                    future: findAllPdfFiles(),
-                    builder: (BuildContext context,
-                        AsyncSnapshot<List<String>> snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return const Center(child: CircularProgressIndicator());
-                      } else if (snapshot.hasError) {
-                        return Center(child: Text(snapshot.error.toString()));
-                      } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                        return const Center(
-                            child: Text('Tidak ada file PDF yang ditemukan.'));
-                      } else {
-                        List<String> _pdffiles = snapshot.data!;
-                        ListView.builder(
-                            itemCount: _pdffiles.length,
-                            itemBuilder: (context, index) {
-                              return ListTile(
-                                title: Text(_pdffiles[index]),
-                              );
-                            });
-                      }
-                      return Container();
-                    }))
+              future: findAllPdfFiles(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<List<PdfFile>> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text(snapshot.error.toString()));
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return const Center(
+                      child: Text('Tidak ada file PDF yang ditemukan.'));
+                } else {
+                  List<PdfFile> _pdffiles = snapshot.data!;
+                  List<PdfFile> _displayedFiles =
+                      _pdffiles.take(_displayedFileCount).toList();
+
+                  // Mengembalikan ListView jika ada file PDF
+                  return Column(children: [
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: _displayedFiles.length,
+                        itemBuilder: (context, index) {
+                          String _pdfPath = _pdffiles[index].path;
+                          String _pdfCreateTime =
+                              _pdffiles[index].createdTime.toString();
+                          int _lastIndex = _pdfPath.split("/").length - 1;
+
+                          return ListTile(
+                            title: Text(_pdffiles[index]
+                                .path
+                                .split("/")[_lastIndex]
+                                .toString()),
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          PdfViewPage(filePath: _pdfPath)));
+                            },
+                          );
+                        },
+                      ),
+                    ),
+                    if (_displayedFiles.length < _pdffiles.length)
+                      TextButton(
+                        child: Text("Selengkapnya"),
+                        onPressed: () {
+                          setState(() {
+                            _displayedFileCount += 10;
+                          });
+                        },
+                      )
+                  ]);
+                }
+              },
+            ))
           ],
         ),
       ),
